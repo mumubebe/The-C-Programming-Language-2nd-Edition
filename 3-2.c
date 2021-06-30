888757c2c6529fcf7e8a0cc417a5cbd92ad50a86
mumubebe <>
2021-06-30 16:30:01 +0200

    3-2

--- 3-2.c
+++ 3-2.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 void escape(char s[], char t[]);
+void unescape(char s[], char t[]);
 
 int main() {
     char t[] = "hello\t\nwat";
     char s[255];
-    escape(s,t);
+    unescape(s,t);
     printf("%s", s);
 }
 
@@ -31,5 +32,33 @@ void escape(char s[], char t[]) {
     
     }
     s[j] = '\0';
+}
 
+void unescape(char s[], char t[]) {
+    int i, j;
+    i = j = 0;
+    while (t[i] != '\0') {
+        switch (t[i]) {
+            case '\\':
+                switch (t[i++]) {
+                    case 't':
+                        s[j] = '\t';
+                        break;
+                    case 'n':
+                        s[j] = '\n';
+                        break;
+                    default:
+                        s[j] = t[i];
+                        break;
+                }
+                
+            default:
+               s[j] = t[i];
+               break;
+        
+        }
+        j++;
+        i++;
+    }
+    s[j] = '\0';
 }
